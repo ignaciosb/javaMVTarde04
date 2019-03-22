5ec23985001bcce4db2bfacc9b96bd6e340a3522
ignaciosb <ignacio@sabate.com.ar>
2019-03-22 17:14:20 -0300

    Update Init.java

--- src/juegoavion/Init.java
+++ src/juegoavion/Init.java
@@ -1,8 +1,13 @@
-package juegoavion;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piloto;
 
 import java.util.ArrayList;
 
-class PilotoAvion {    
+class PilotoAvion {
     public PilotoAvion(String nombre, int cantidadDeChoques, boolean leGustaVolar) {
         this.nombre = nombre;
         this.cantidadDeChoques = cantidadDeChoques;
@@ -13,21 +18,55 @@ class PilotoAvion {
     public final boolean leGustaVolar;
 }
 
-public class Init {
-    public static ArrayList<PilotoAvion> obtenerAquellosQueLesGustaVolarYTienenAccidentes() {
-        return null;
+/**
+ *
+ * @author EducaciónIT
+ */
+public class Piloto {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static ArrayList<PilotoAvion> obtenerAquellosQueLesGustaVolarYTienenAccidentes(ArrayList<PilotoAvion> listaCompleta) {
+        ArrayList<PilotoAvion> pList = new ArrayList<PilotoAvion>();
+        for(PilotoAvion p:listaCompleta) {
+            if(p.leGustaVolar && p.cantidadDeChoques > 0) {
+                pList.add(p);
+            }
+        }
+        return pList;
     }
-    public static ArrayList<PilotoAvion> obtenerAquellosQueLesGustaVolarYNOTienenAccidentes() {
-        return null;
+    public static ArrayList<PilotoAvion> obtenerAquellosQueLesGustaVolarYNOTienenAccidentes(ArrayList<PilotoAvion> listaCompleta) {
+        ArrayList<PilotoAvion> pList = new ArrayList<PilotoAvion>();
+        for(PilotoAvion p:listaCompleta) {
+            if(p.leGustaVolar && p.cantidadDeChoques == 0) {
+                pList.add(p);
+            }
+        }
+        return pList;
     }
-    public static ArrayList<PilotoAvion> obtenerAquellosQueNOLesGustaVolarYTienenAccidentes() {
-        return null;
+    public static ArrayList<PilotoAvion> obtenerAquellosQueNOLesGustaVolarYTienenAccidentes(ArrayList<PilotoAvion> listaCompleta) {
+        ArrayList<PilotoAvion> pList = new ArrayList<PilotoAvion>();
+        for(PilotoAvion p:listaCompleta) {
+            if((!p.leGustaVolar) && (p.cantidadDeChoques > 0)) {
+                pList.add(p);
+            }
+        }
+        return pList;
     }
-    public static ArrayList<PilotoAvion> obtenerAquellosQueNOLesGustaVolarYNOTienenAccidentes() {
-        return null;
+    public static ArrayList<PilotoAvion> obtenerAquellosQueNOLesGustaVolarYNOTienenAccidentes(ArrayList<PilotoAvion> listaCompleta) {
+        ArrayList<PilotoAvion> pList = new ArrayList<PilotoAvion>();
+        for(PilotoAvion p:listaCompleta) {
+            if((!p.leGustaVolar) && (p.cantidadDeChoques == 0)) {
+                pList.add(p);
+            }
+        }
+        return pList;
     }
     public static void mostrarLista(ArrayList<PilotoAvion> pilotos) {
-        
+        for(PilotoAvion p:pilotos) {
+            System.out.println(p.nombre);
+        }
     }
     public static ArrayList<PilotoAvion> crearLosPilotos() {
         ArrayList<PilotoAvion> pilotos = new ArrayList<PilotoAvion>();
@@ -40,10 +79,27 @@ public class Init {
         pilotos.add(new PilotoAvion("Pedro", 0, true));
         pilotos.add(new PilotoAvion("Martin", 6, false));
         pilotos.add(new PilotoAvion("Mario", 3, true));
-        
+
         return pilotos;
     }
+    
     public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList<PilotoAvion> pilotos = new ArrayList<PilotoAvion>();
+        pilotos = crearLosPilotos();
+        System.out.println("NO les Gusta Volar y NO Tienen accidentes: ");
+        mostrarLista(obtenerAquellosQueNOLesGustaVolarYNOTienenAccidentes(pilotos));
+        
+        System.out.println("NO les Gusta Volar y Tienen accidentes: ");
+        mostrarLista(obtenerAquellosQueNOLesGustaVolarYTienenAccidentes(pilotos));
+        
+        System.out.println("Les Gusta Volar y NO Tienen accidentes: ");
+        mostrarLista(obtenerAquellosQueLesGustaVolarYNOTienenAccidentes(pilotos));
+        
+        System.out.println("Les Gusta Volar y Tienen accidentes: ");
+        mostrarLista(obtenerAquellosQueLesGustaVolarYTienenAccidentes(pilotos));
         
     }
+    
 }
